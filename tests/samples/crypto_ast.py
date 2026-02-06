24f61838a0577a57da86dd8f167c7ada14f23b7b
Waqas <waqasobeidy@gmail.com>
2026-02-06 23:28:47 +0800

    Add tests/samples/ to gitignore

--- tests/samples/crypto_ast.py
+++
@@ -1,9 +0,0 @@
-from cryptography.hazmat.primitives.asymmetric import rsa
-
-def generate_key():
-    # This should be caught by ASTScanner
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-    )
-    return private_key
--- tests/samples/go.mod
+++
@@ -1,8 +0,0 @@
-module example.com/m
-
-go 1.21
-
-require (
-    crypto/rsa v0.0.0
-    crypto/ecdsa v0.0.0
-)
